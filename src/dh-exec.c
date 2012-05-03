429c46d0f9bf90ddbd281134b5c732a0c40761bd
Gergely Nagy <algernon@balabit.hu>
2012-05-03 12:45:32 +0200

    dh-exec: Bump the copyright year in the --version output
    Signed-off-by: Gergely Nagy <algernon@balabit.hu>

--- src/dh-exec.c
+++ src/dh-exec.c
@@ -134,7 +134,7 @@ dh_exec_version (void)
 {
   printf ("dh-exec " PACKAGE_VERSION "\n"
           "\n"
-          "Copyright (C) 2011 Gergely Nagy <algernon@debian.org>\n"
+          "Copyright (C) 2011-2012 Gergely Nagy <algernon@debian.org>\n"
           "This is free software; see the source for copying conditions.  There is NO\n"
           "warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n"
           "\n"
